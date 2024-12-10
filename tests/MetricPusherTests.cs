cf973d852ab0e455ebac1c4233a42e4e3ea2feba
Serge K <cmt@phnx47.net>
2024-12-10 21:57:27 +0700

    add asserts to tests

--- tests/MetricPusherTests.cs
+++ tests/MetricPusherTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Prometheus.Client.Collectors;
 using Xunit;
@@ -12,19 +14,33 @@ public class MetricPusherTests(PushGatewayFixture fixture, ITestOutputHelper out
     private readonly IMetricFactory _metricFactory = new MetricFactory(new CollectorRegistry());
 
     [Fact]
-    public async Task Push()
+    public async Task PushWithoutException()
     {
-        var counter = _metricFactory.CreateCounter("test_c12", "help");
+        var counter = _metricFactory.CreateCounter("counter1", "help");
         counter.Inc();
 
         var pusher = new MetricPusher(new MetricPusherOptions { Endpoint = _endpoint, Job = "pushgateway-test", Instance = "instance" });
-        await pusher.PushAsync();
+
+        var ex = await Record.ExceptionAsync(() => pusher.PushAsync());
+        Assert.Null(ex);
     }
 
     [Fact]
-    public async Task PushWithAdditionalHeaders()
+    public async Task PushToWrongUrlWithHttpRequestException()
     {
-        var counter = _metricFactory.CreateCounter("test_", "help");
+        var counter = _metricFactory.CreateCounter("counter2", "help");
+        counter.Inc();
+
+        var pusher = new MetricPusher(new MetricPusherOptions { Endpoint = "http://localhost:2543", Job = "pushgateway-test", Instance = "instance" });
+
+        var ex = await Record.ExceptionAsync(() => pusher.PushAsync());
+        Assert.IsType<HttpRequestException>(ex);
+    }
+
+    [Fact]
+    public async Task PushWithAdditionalHeadersWithoutException()
+    {
+        var counter = _metricFactory.CreateCounter("counter3", "help");
         counter.Inc();
 
         const string accessToken = "";
@@ -35,22 +51,42 @@ public class MetricPusherTests(PushGatewayFixture fixture, ITestOutputHelper out
             Instance = "instance",
             AdditionalHeaders = new Dictionary<string, string> { { "Authorization", "Bearer " + accessToken } }
         });
-        await pusher.PushAsync();
+
+        var ex = await Record.ExceptionAsync(() => pusher.PushAsync());
+        Assert.Null(ex);
     }
 
     [Fact]
-    public async Task Worker10Steps()
+    public async Task Worker10StepsWithExpectedResult()
     {
         var counter = _metricFactory.CreateCounter("worker_counter1", "help");
         var pusher = new MetricPusher(new MetricPusherOptions { Endpoint = _endpoint, Job = "pushgateway-testworker" });
-
         var worker = new MetricPushServer(pusher);
+
+        var resultBuilder = new StringBuilder();
+        const string expectedResult = """
+                                      Step: 0, IsRunning: True
+                                      Step: 1, IsRunning: True
+                                      Step: 2, IsRunning: True
+                                      Step: 3, IsRunning: True
+                                      Step: 4, IsRunning: True
+                                      Step: 5, IsRunning: True
+                                      Step: 6, IsRunning: False
+                                      Step: 7, IsRunning: False
+                                      Step: 8, IsRunning: False
+                                      Step: 9, IsRunning: True
+
+                                      """;
+
         worker.Start();
 
         for (int i = 0; i < 10; i++)
         {
+            var info = $"Step: {i}, IsRunning: {worker.IsRunning}";
+            resultBuilder.AppendLine(info);
+            output.WriteLine(info);
+
             counter.Inc();
-            output.WriteLine($"Step: {i}, IsRunning: {worker.IsRunning}");
 
             switch (i)
             {
@@ -64,6 +100,8 @@ public class MetricPusherTests(PushGatewayFixture fixture, ITestOutputHelper out
 
             await Task.Delay(100);
         }
+
+        Assert.Equal(expectedResult, resultBuilder.ToString());
 
         worker.Stop();
     }
