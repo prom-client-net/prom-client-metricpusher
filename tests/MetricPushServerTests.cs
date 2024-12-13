99e0f73ad724ac6d40599a89014b33f58745fa77
Serge K <cmt@phnx47.net>
2024-12-13 21:44:11 +0700

    use default push internal in tests

--- tests/MetricPushServerTests.cs
+++ tests/MetricPushServerTests.cs
@@ -11,11 +11,11 @@ public class MetricPushServerTests
     public async Task PushContinuesOnError()
     {
         var pusher = Substitute.For<IMetricPusher>();
-        pusher.PushAsync().Returns(Task.FromException(new Exception("Push error")));
+        pusher.PushAsync().Returns(Task.FromException(new Exception("Simulated Push Error")));
 
-        var worker = new MetricPushServer(pusher, TimeSpan.FromSeconds(0.05));
+        var worker = new MetricPushServer(pusher);
         worker.Start();
-        await Task.Delay(150);
+        await Task.Delay(2500);
 
         await pusher.Received(3).PushAsync();
         worker.Stop();
