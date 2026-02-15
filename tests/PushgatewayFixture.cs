ce82ef2ddbe322b19c21fc4ef4eb65893ce78331
Serge K <cmt@phnx47.net>
2026-02-15 17:29:05 +0700

    Update PushgatewayFixture

--- tests/PushgatewayFixture.cs
+++ tests/PushgatewayFixture.cs
@@ -8,16 +8,23 @@ namespace Prometheus.Client.MetricPusher.Tests;
 
 public class PushgatewayFixture : IAsyncLifetime
 {
-    private readonly IContainer _container = new ContainerBuilder()
-        .WithImage("prom/pushgateway")
+    private readonly IContainer _container = new ContainerBuilder("prom/pushgateway")
         .WithPortBinding(9091, true)
         .Build();
 
     public string GetEndpoint() => $"{Uri.UriSchemeHttp}://{_container.Hostname}:{_container.GetMappedPublicPort(9091)}";
 
+#if NET462
     public Task InitializeAsync()
         => _container.StartAsync();
 
     public Task DisposeAsync()
         => _container.DisposeAsync().AsTask();
+#else
+    public async ValueTask InitializeAsync()
+        => await _container.StartAsync();
+
+    public async ValueTask DisposeAsync()
+        => await _container.DisposeAsync();
+#endif
 }
